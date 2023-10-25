86d3d86ebad5da20975893854c103b5b7c4c8a1f
oksanadrohobych <147349912+oksanadrohobych@users.noreply.github.com>
2023-10-25 18:37:17 -0400

    Update heap_sort.py

--- heap_sort.py
+++ heap_sort.py
@@ -1,23 +1,21 @@
 def heapify(arr, N, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1     # left = 2*i + 1
-    r = 2 * i + 2     # right = 2*i + 2
+    largest = i 
+    l = 2 * i + 1  
+    r = 2 * i + 2  
  
-    # See if left child of root exists and is
-    # greater than root
+    
     if l < N and arr[largest] < arr[l]:
         largest = l
  
-    # See if right child of root exists and is
-    # greater than root
+    
     if r < N and arr[largest] < arr[r]:
         largest = r
  
-    # Change root, if needed
+    
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]  # swap
  
-        # Heapify the root.
+    
         heapify(arr, N, largest)
  
 # The main function to sort an array of given size
